43ac2d6248b01d980fbaefa02dc6bba927fdc689
dickmao <none>
2020-10-17 15:24:30 -0400

    package-lint insists on minor version number

--- lisp/nntwitter-pkg.el
+++ lisp/nntwitter-pkg.el
@@ -1,7 +1,7 @@
 (define-package "nntwitter"
   "0.1.0"
   "Gnus Backend For Twitter"
-  '((emacs "25")
+  '((emacs "25.1")
     (dash "20190401")
     (anaphora "20180618")
     (request "20190819"))
--- tools/melpazoid.sh
+++ tools/melpazoid.sh
@@ -16,8 +16,9 @@ ROOT=$(git rev-parse --show-toplevel)
 cd ${ROOT}
 PKG_PATH="${ROOT}/melpazoid-master/$(basename $(pwd))"
 PKG_NAME=$(basename "$PKG_PATH")
+PKG_MAIN=$(cask files | egrep -- "pkg.el$" | xargs basename 2>/dev/null)
 mkdir -p ${PKG_PATH}
-rsync -av --exclude '*autoloads.el' --exclude '*pkg.el' $(cask files) ${PKG_PATH} --delete
+rsync -av --exclude '*autoloads.el' $(cask files) ${PKG_PATH} --delete
 if [ -s "${ROOT}/LICENSE" ]; then
   cp -p "${ROOT}/LICENSE" ${PKG_PATH}
 fi
@@ -30,4 +31,4 @@ if [ ! -s ./python ]; then rm -f ./python ; ln -s $PYTHON ./python ; fi
 
 # Later: melpazoid is now an elisp file?
 cd ${ROOT}
-EMACS=$EMACS cask emacs -Q --batch -l package --eval "(setq package-user-dir \"$(cask package-directory)\")" -f package-initialize -L ${ROOT}/melpazoid-master/melpazoid --eval "(setq default-directory \"${PKG_PATH}\")" --eval "(let (eval-expression-print-length eval-expression-print-level) (prin1 package-user-dir (function external-debugging-output)) (prin1 package-directory-list (function external-debugging-output)) (prin1 package-alist (function external-debugging-output)))" -l melpazoid
+PACKAGE_MAIN=$PKG_MAIN EMACS=$EMACS cask emacs -Q --batch -l package --eval "(setq package-user-dir \"$(cask package-directory)\")" -f package-initialize -L ${ROOT}/melpazoid-master/melpazoid --eval "(setq default-directory \"${PKG_PATH}\")" --eval "(let (eval-expression-print-length eval-expression-print-level) (prin1 package-user-dir (function external-debugging-output)) (prin1 package-directory-list (function external-debugging-output)) (prin1 package-alist (function external-debugging-output)))" -l melpazoid
